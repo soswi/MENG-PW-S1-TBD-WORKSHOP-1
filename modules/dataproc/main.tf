1ef89e6221fa641ce46db54bbd3be41f6a0ab043
soswi <wiktorsoswisosnowski@gmail.com>
2026-05-10 10:35:51 +0000

    feat: add spot instances to dataproc and complete tasks 5-11 docs

--- modules/dataproc/main.tf
+++ modules/dataproc/main.tf
@@ -134,5 +134,15 @@ resource "google_dataproc_cluster" "tbd-dataproc-cluster" {
       }
 
     }
+
+    preemptible_worker_config {
+      num_instances  = 1
+      preemptibility = "SPOT"
+      disk_config {
+        boot_disk_type    = "pd-standard"
+        boot_disk_size_gb = 100
+      }
+    }
+
   }
 }
